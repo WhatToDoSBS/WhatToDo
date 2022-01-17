85ec82b0571d027deb631995822ba09d6eb8afa0
sengwanchoi <wan7523376@naver.com>
2022-01-17 15:59:53 +0900

    board 내용 채워놓음

---
+++ out/artifacts/WhatToDo_war_exploded/res/js/user/join.js
@@ -0,0 +1,34 @@
+const idRegex = /^([a-zA-Z0-9]{4,15})$/; //대소문자+숫자 조합으로 4~15글자인 경우만 OK!
+const pwRegex = /^([a-zA-Z0-9!@_]{4,20})$/; //대소문자+숫자+!@_ 조합으로 4~20글자인 경우만 OK!
+const nmRegex = /^([가-힣]{2,5})$/;
+const joinFrmElem = document.querySelector('#join_frm');
+
+joinFrmElem.addEventListener('submit', (e) => {
+    const uid = joinFrmElem.uid.value;
+    const upw = joinFrmElem.upw.value;
+    const upwChk = joinFrmElem.querySelector('#upw-chk').value;
+    const nm = joinFrmElem.nm.value;
+
+    if (!idRegex.test(uid)) {
+        alert('아이디는 대소문자, 숫자조합으로 4~15글자가 되어야 합니다.');
+    }else if (!pwRegex.test(upw)) {
+        alert('비밀번호는 대소문자, 숫자, !@_ 조합으로 4~20글자가 되어야 합니다.');
+        e.preventDefault();
+    } else if(upw !== upwChk) {
+        alert('비밀번호와 체크 비밀번호를 확인해 주세요.');
+        e.preventDefault();
+    } else if(!nmRegex.test(nm)) {
+        alert('이름은 한글로 2~5글자가 되어야 합니다.');
+        e.preventDefault();
+    } else if(idChkState !== 1) {
+        switch (idChkState) {
+            case 0:
+                alert('다른 아이디를 사용해 주세요!');
+                break;
+            case 2:
+                alert('아이디 중복 체크를 해주세요!');
+                break;
+        }
+        e.preventDefault();
+    }
+});
