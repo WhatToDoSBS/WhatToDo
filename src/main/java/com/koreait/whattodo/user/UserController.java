0ff3db0c58bb22129410952ed8015c44e0ca2632
sengwanchoi <wan7523376@naver.com>
2022-01-17 09:48:12 +0900

    결과처리 작업중

--- src/main/java/com/koreait/whattodo/user/UserController.java
+++ src/main/java/com/koreait/whattodo/user/UserController.java
@@ -3,9 +3,12 @@ package com.koreait.whattodo.user;
 import com.koreait.whattodo.model.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -18,23 +21,42 @@ public class UserController {
     public void login() {}
 
     @PostMapping("/login")
-    public String loginPost(UserEntity entity) {
-        UserEntity result = service.login(entity);
-        if (result == null) {
-            return "redirect:/user/login";
+    public String loginPost(UserEntity entity, HttpSession session, Model model) {
+        UserEntity loginUser = (UserEntity) session.getAttribute("loginUser");
+        // 로그인한 유저의 경우 로그인창으로 접근 막음
+        if (loginUser != null) {
+            return "redirect:/board/list";
         }
-        return "redirect:/board/list";
+
+        int result = service.login(entity);
+        switch (result) {
+            case 1:
+                return "redirect:/board/list";
+            case 2:
+                model.addAttribute("msg", "로그인에 실패하였습니다.");
+                return "redirect:/user/login";
+        }
+        model.addAttribute("msg", "계정이 존재하지 않습니다.");
+        return "redirect:/user/login";
     }
 
     @GetMapping("/join")
     public void join() {}
 
     @PostMapping("/join")
-    public String joinPost(UserEntity entity) {
+    public String joinPost(UserEntity entity, HttpSession session, Model model) {
+        UserEntity loginUser = (UserEntity) session.getAttribute("loginUser");
+        // 로그인 세션을 가져와서 로그인한 유저일경우 회원가입접근 막음
+        if (loginUser != null) {
+            return "redirect:/board/list";
+        }
+
         int result = service.join(entity);
         if (result == 1) {
             return "redirect:/user/login";
         }
+        model.addAttribute("msg", "회원가입에 실패하였습니다.");
         return "redirect:/user/join";
     }
+
 }
--- web/WEB-INF/views/user/join.jsp
+++ web/WEB-INF/views/user/join.jsp
@@ -2,7 +2,11 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <div>
     <h1>회원가입</h1>
+
+    <form action="/user/join" method="post" id="join_frm">
+=======
     <form action="/user/join" method="post">
+
         <div>ID : <input type="text" name="uid"></div>
         <div>PW : <input type="password" name="upw"></div>
         <div>PW-Check : <input type="password" name="upw-check"></div>
@@ -11,8 +15,12 @@
             gender : <input type="radio" name="gender" value="1"> 남성
             <input type="radio" name="gender" value="2"> 여성
         </div>
+
         <div>
             <input type="submit" value="회원가입">
         </div>
+
+        <div></div>
+
     </form>
 </div>
