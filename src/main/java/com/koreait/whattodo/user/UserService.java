11a32587f48d44f7271ae4b52cf4903ae9c8a3d8
sengwanchoi <wan7523376@naver.com>
2022-01-20 14:44:46 +0900

    session login 정보 추가

--- src/main/java/com/koreait/whattodo/user/UserService.java
+++ src/main/java/com/koreait/whattodo/user/UserService.java
@@ -4,11 +4,16 @@ import com.koreait.whattodo.model.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserMapper mapper;
+
+    @Autowired
+    private HttpSession session;
 
     public int join(UserEntity entity) {
         return mapper.insUser(entity);
@@ -19,6 +24,7 @@ public class UserService {
         if (result == null) {
             return 0; // 로그인 실패
         } else if (result.getUpw().equals(entity.getUpw())) {
+            session.setAttribute("loginUser", result);
             return 1; // 성공
         }
         return 2; // 비번 오류
