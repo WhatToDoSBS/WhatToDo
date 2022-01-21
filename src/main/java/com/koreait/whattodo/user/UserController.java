1e00bdd87055518515b0026bb0dedaa48bd4ea54
juyoung <terranzz01@gmail.com>
2022-01-21 14:38:11 +0900

    게시판 페이징 작업 중

--- src/main/java/com/koreait/whattodo/user/UserController.java
+++ src/main/java/com/koreait/whattodo/user/UserController.java
@@ -65,10 +65,5 @@ public class UserController {
         return "redirect:/user/join";
     }
 
-    @GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate();
-        return "redirect:/user/login";
-    }
 
 }
