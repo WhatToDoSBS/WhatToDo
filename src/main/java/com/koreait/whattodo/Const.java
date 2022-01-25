568cbec685597e12f532677eb2368d137ddb7118
StarBamBi <59411333+StarBamBi@users.noreply.github.com>
2022-01-25 22:21:05 +0900

    네이버 캡쳐 api 테스트 중

--- src/main/java/com/koreait/whattodo/Const.java
+++ src/main/java/com/koreait/whattodo/Const.java
@@ -2,4 +2,7 @@ package com.koreait.whattodo;
 
 public class Const {
     public static final String MENU_LIST = "menuList";
+    public static final String BOOK_KEY = "tttbwhiteme061607001";
+    public static final String NAVER_SECRET = "29LoNGyHf2";
+    public static final String CLIENT_ID = "hFpuwARjo0tOJCRMoKeo";
 }
