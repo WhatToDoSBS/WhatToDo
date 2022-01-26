3a8307e113e1d4ce81536bada1b898f6f24d4f6a
StarBamBi <whiteme06@gmail.com>
2022-01-26 16:08:58 +0900

    웹툰 작업중, 웹툰 크롤링해서 데이터베이스에 담기 성공

--- src/main/java/com/koreait/whattodo/webtoon/WebtoonController.java
+++ src/main/java/com/koreait/whattodo/webtoon/WebtoonController.java
@@ -1,4 +1,25 @@
 package com.koreait.whattodo.webtoon;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/board", produces = "application/text; charset=UTF-8") // js 한글깨짐 방지
 public class WebtoonController {
+    @Autowired
+    private WebtoonService service;
+
+    @GetMapping("/webtoon")
+    public void webtoon() {}
+
+    @GetMapping("/webtooncrawling")
+    public String webtoonCrawling() {
+        String naverWebtoonURL = "https://comic.naver.com/webtoon/weekdayList?week=mon&order=User&view=image";
+        service.delWebtoon();
+        service.insertWebtoon(naverWebtoonURL);
+
+        return "redirect:/board/webtoon";
+    }
 }
--- src/main/java/com/koreait/whattodo/webtoon/WebtoonService.java
+++ src/main/java/com/koreait/whattodo/webtoon/WebtoonService.java
@@ -1,4 +1,77 @@
 package com.koreait.whattodo.webtoon;
 
+import com.koreait.whattodo.model.WebtoonEntity;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class WebtoonService {
+    @Autowired
+    private WebtoonMapper mapper;
+
+    public void insertWebtoon(String url) {
+
+        // 액세스 처리
+        try {
+            Document document = Jsoup.connect(url).userAgent("Chrome/5.0").get(); // 403 error 처리(권한 부여)
+
+            Elements toonNm = document.select("ul.img_list").select("dt > a"); // 웹툰 이름
+            Elements toonWriter = document.select("ul.img_list").select("dd.desc > a");   // 작가
+            Elements toonRating = document.select("ul.img_list").select("dd > div.rating_type").select("strong");    // 별점
+            Elements toonImg = document.select("ul.img_list").select("div.thumb > a > img");    // PC게임 평점
+
+            List toonNmList = new ArrayList();
+            List toonWriterList = new ArrayList();
+            List toonRatingList = new ArrayList();
+            List toonImgList = new ArrayList();
+
+            for(Element element: toonNm) {    // 웹툰 이름
+                String name = element.getElementsByAttribute("title").attr("title");
+                toonNmList.add(name);
+            }
+            for(Element element: toonWriter) {    // 작가이름
+                String name = element.text();
+                toonWriterList.add(name);
+            }
+            for(Element element: toonRating) {    // 별점
+                String name = element.text();
+                toonRatingList.add(name);
+            }
+            for(Element element: toonImg) {    // PC게임 이름 크롤링
+                String name = element.getElementsByAttribute("src").attr("src");
+                toonImgList.add(name);
+            }
+
+            System.out.println("toonNmList" + toonNmList);
+            System.out.println("toonWriterList" + toonWriterList);
+            System.out.println("toonRatingList" + toonRatingList);
+            System.out.println("toonImgList" + toonImgList);
+
+
+            for(int i=0;i<toonNmList.size();i++) {
+                List<WebtoonEntity> list = new ArrayList<>();
+                WebtoonEntity entity = new WebtoonEntity();
+
+                entity.setNm((String)toonNmList.get(i));
+                entity.setWriter((String)toonWriterList.get(i));
+                entity.setRating((String)toonRatingList.get(i));
+                entity.setImg((String)toonImgList.get(i));
+                list.add(entity);
+                mapper.insertWebtoon(list);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delWebtoon() { mapper.delWebtoon(); }
 }
