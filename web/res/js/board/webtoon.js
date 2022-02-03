a6c0445cf5425bcdafa6e15fcc0fd582659826c2
StarBamBi <59411333+StarBamBi@users.noreply.github.com>
2022-02-04 04:03:49 +0900

    웹툰 random 버튼 작업 중 / css 조절

--- web/res/js/board/webtoon.js
+++ web/res/js/board/webtoon.js
@@ -14,16 +14,17 @@ function webtoonGenreRandom(url) {
         return res.json();
     }).then((data) => {
         let randomNum = Math.floor((Math.random() * data.length));  // 랜덤 숫자
-        console.log(data[randomNum]);  // 사이즈 구하는 법 : Object.keys(data).length
-        resultDisplay(data[randomNum].img,data[randomNum].link,data[randomNum].nm,data[randomNum].writer);
+        console.log(data);  // 사이즈 구하는 법 : Object.keys(data).length
+        resultDisplay(data[randomNum].img,data[randomNum].link,data[randomNum].nm,data[randomNum].writer, data[randomNum].genre);
     }).catch((err) => {
         console.log(err);
     });
 }
-function resultDisplay(webtoonimg, webtoonLink, webtoonNm, webtoonWriter) {
+function resultDisplay(webtoonimg, webtoonLink, webtoonNm, webtoonWriter, webtoonGenre) {
     resultBox.innerHTML = `<div><a href="${webtoonLink}"><img src="${webtoonimg}"></a></div>
     <div><a href="${webtoonLink}"><span class="font-14px">${webtoonNm}</span></a></div>
     <div><span class="font-14px">${webtoonWriter}</span></div>
+    <div><span class="font-14px">${webtoonGenre}</span></div>
 `;
 }
 randomSubmitBtn.addEventListener('click',function () {
