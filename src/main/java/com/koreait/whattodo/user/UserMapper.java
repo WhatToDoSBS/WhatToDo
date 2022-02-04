7594a79602ba04019c3f507b3b8c12878edc7e9e
sengwanchoi <wan7523376@naver.com>
2022-02-04 14:37:14 +0900

    주석 + 회원가입 css 수정

--- src/main/java/com/koreait/whattodo/user/UserMapper.java
+++ src/main/java/com/koreait/whattodo/user/UserMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    int insUser(UserEntity entity);
+    int insUser(UserEntity entity); // 회원가입
 
-    UserEntity selUser(UserEntity entity);
+    UserEntity selUser(UserEntity entity); // 로그인
 }
