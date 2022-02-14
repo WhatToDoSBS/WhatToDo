8da04666a2aceb60554477cd91166fd3e11f807a
sengwanchoi <wan7523376@naver.com>
2022-02-14 16:48:04 +0900

    네이버 로그인 if 처리중

--- web/res/js/user/login.js
+++ web/res/js/user/login.js
@@ -1,5 +1,6 @@
-var naver_id_login = new naver_id_login("s8DDAYotRndJt05wXdOI", "http://localhost:8090/board/main");
+var naver_id_login = new naver_id_login("9CbEg9cxRUs7V2Q6_IMd", "http://localhost:8090/user/naver/callback");
 var state = naver_id_login.getUniqState();
+console.log(state);
 naver_id_login.setButton("green", 3,60);
 naver_id_login.setDomain("http://localhost:8090/user/login");
 naver_id_login.setState(state);
