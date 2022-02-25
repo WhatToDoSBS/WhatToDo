e381c04c401e79fff57bb6cdbe4e7a93e03207de
sengwanchoi <wan7523376@naver.com>
2022-02-25 16:44:54 +0900

    프로필 이미지 값 전송 완료

--- src/main/java/com/koreait/whattodo/user/mypage/UserMypageController.java
+++ src/main/java/com/koreait/whattodo/user/mypage/UserMypageController.java
@@ -15,12 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -51,10 +53,15 @@ public class UserMypageController {
         return "redirect:/user/login";
     }
 
-    @PostMapping("/mypage/profileimg")
     @ResponseBody
-    public Map<String, String> profileImgCha(@PathVariable String profileimg) {
-        return null;
+    @PostMapping("/mypage/profileimg")
+    public Map<String, String> profileImgCha(MultipartFile profileimg) {
+        System.out.println("profileimg : " + profileimg);
+
+
+        Map<String, String> res = new HashMap();
+        res.put("image", "profileimg");
+        return res;
     }
 
     @PostMapping("/mypage/cheUpw")
--- web/res/js/mypage/main.js
+++ web/res/js/mypage/main.js
@@ -2,32 +2,39 @@
     const fileInputBtn = document.querySelector('#input_file');
     const fileInputLabel = document.querySelector('#input_for_label');
 
-    fileInputLabel.addEventListener('click', () => {
-        if (fileInputBtn) {
-            fileInputBtn.click();
-        }
-    });
+    if (fileInputLabel) {
+        fileInputLabel.addEventListener('click', () => {
+            if (fileInputBtn) {
+                fileInputBtn.click();
+            }
+        });
+    }
 
-    fileInputBtn.addEventListener('change', () => {
-        const img = fileInputBtn.files[0];
-        if (img != null) {
-            uploadProfileImg(img);
-        }
-    });
-
-
+    if (fileInputBtn) {
+        fileInputBtn.addEventListener('change', () => {
+            const img = fileInputBtn.files[0];
+            if (img != null) {
+                uploadProfileImg(img);
+            }
+        });
+    }
 
     const uploadProfileImg = (img) => {
         const fData = new FormData();
         fData.append('profileimg', img);
+        console.log(img);
+
+        for (var pair of fData.entries()) {
+            console.log(pair[0]+ ', ' + pair[1]);
+        }
 
         fetch('/user/mypage/profileimg', {
-            'method': 'post',
-            'body': fData
+            method: 'POST',
+            body: fData,
         })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.image);
             })
             .catch(e => {
                 console.log(e);
